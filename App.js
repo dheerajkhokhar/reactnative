6643e09001269a5adf2d413ab16ed94a8ea67eda
dheerajkumar <dheeraj@SQMLaptop04.local>
2020-05-30 18:11:43 +0530

    Initial push

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+    StyleSheet,
+    Dimensions,
+    Text,
+    View,
+    SafeAreaView,
+    Image,
+    TouchableWithoutFeedback,
+    TouchableOpacity,
+    TouchableHighlight,
+    Button,
+    Alert,
+    StatusBar,
+    Platform
+} from "react-native";
+import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
+
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    // console.log(Dimensions.get("screen"));
+    console.log(useDimensions(), useDeviceOrientation());
+
+    const { landscape } = useDeviceOrientation();
+    return (
+        <View style={styles.container}>
+            {/* <WelcomeScreen></WelcomeScreen> */}
+            <ViewImageScreen></ViewImageScreen>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        // alignItems: "center",
+        // justifyContent: "center",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    }
 });
